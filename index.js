c085036edd4f7c0141234d37a7b9bffb86a0b2a7
whisnu aries dewantara <dewantara.whisnu@gmail.com>
2024-10-24 21:54:05 +0700

    add security: helmet, cors, body-parser, limiter

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const { appMiddleware } = require("./middleware/index.js");
-const db = require("./db/models/index.js");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
-const app = express();
+const { appMiddleware } = require("./middleware/index.js");
+const db = require("./db/models/index.js");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 menit
+  max: 100, // 100 permintaan
+});
+
 const port = process.env.APP_PORT;
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(limiter);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.multipart());
 
 app.get("/", appMiddleware, (req, res) => {
   res.send("Welcome home!");
