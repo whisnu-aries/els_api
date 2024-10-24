435f51e07c4645f40cac174e73b1c3528b6739f4
whisnu aries dewantara <dewantara.whisnu@gmail.com>
2024-10-24 20:46:23 +0700

    implement sequelize and setup connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const db = require("./db/models/index.js");
+
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +9,15 @@ const port = process.env.APP_PORT;
 app.get("/", (req, res) => {
   res.send("Welcome home!");
 });
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Koneksi ke database berhasil.");
+  })
+  .catch((err) => {
+    console.error("Tidak dapat terhubung ke database:", err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
